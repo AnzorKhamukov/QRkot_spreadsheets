f603bf2643ef42ff3cf39c4fbd12bdb039224272
Anzor Khamukov <AnzorKhamukov@yandex.ru>
2024-01-08 18:18:15 +0300

    hw

---
+++ app/schemas/charity_project.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Extra, Field, PositiveInt
+
+
+class CharityProjectBase(BaseModel):
+    name: Optional[str] = Field(None, max_length=100)
+    description: Optional[str] = Field(None)
+    full_amount: Optional[PositiveInt] = Field(None)
+
+    class Config:
+        extra = Extra.forbid
+        min_anystr_length = 1
+
+
+class CharityProjectCreate(CharityProjectBase):
+    name: str = Field(..., max_length=100)
+    description: str = Field(...)
+    full_amount: int = Field(..., gt=0)
+
+
+class CharityProjectUpdate(CharityProjectBase):
+    pass
+
+
+class CharityProjectDB(CharityProjectBase):
+    id: int
+    invested_amount: int
+    fully_invested: bool
+    create_date: datetime
+    close_date: datetime = Field(None)
+
+    class Config:
+        orm_mode = True
